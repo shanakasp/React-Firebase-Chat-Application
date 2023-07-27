e3b44e66750a755979c6e29726c7972dfb8e1b80
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-27 16:36:57 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,13 +7,13 @@ import {getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYrpq_N9xLXdqx1u7a4dcYw5829g6UGU4",
-  authDomain: "react-chat-app-a1895.firebaseapp.com",
-  projectId: "react-chat-app-a1895",
-  storageBucket: "react-chat-app-a1895.appspot.com",
-  messagingSenderId: "16929318379",
-  appId: "1:16929318379:web:40a0da91bb8064bfdbe5ae",
-  measurementId: "G-WT80F05Z4Y"
+  apiKey: "*",
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*",
+  measurementId: "*"
 };
 
 // Initialize Firebase
